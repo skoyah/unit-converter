2dc7662db88f2923c3755916f651cf2e6f5e7ab8
João Santos <joaoecoceanus@gmail.com>
2018-10-23 12:36:42 +0100

    Initial commit

---
+++ tests/ExampleTest.php
@@ -0,0 +1,12 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+
+class ExampleTest extends TestCase
+{
+    /** @test */
+    public function example_test()
+    {
+        $this->assertTrue(true);
+    }
+}
