4572d15896a66e9719d4a8f3e8ed56d450e3032f
skoyah <joaoecoceanus@gmail.com>
2018-11-07 12:53:36 +0000

    Apply fixes from StyleCI

--- src/config/temperature.php
+++ src/config/temperature.php
@@ -28,7 +28,11 @@ return [
 
     'formulas' => [
         'k' => 1, // SI Unit
-        'c' => function ($value, $toBaseUnit = false) { return $toBaseUnit ? $value + 273.15 : $value - 273.15; },
-        'f' => function ($value, $toBaseUnit = false) { return $toBaseUnit ? ($value + 459.67) * 5 / 9 : ($value * 9 / 5) - 459.67; },
+        'c' => function ($value, $toBaseUnit = false) {
+            return $toBaseUnit ? $value + 273.15 : $value - 273.15;
+        },
+        'f' => function ($value, $toBaseUnit = false) {
+            return $toBaseUnit ? ($value + 459.67) * 5 / 9 : ($value * 9 / 5) - 459.67;
+        },
     ],
 ];
