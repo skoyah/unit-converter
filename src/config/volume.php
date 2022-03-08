5beb8b00eb131969e0058d994c6d53d34c8549ff
João Santos <36083448+skoyah@users.noreply.github.com>
2022-03-08 11:06:01 +0100

    Apply fixes from StyleCI (#12)

--- src/config/volume.php
+++ src/config/volume.php
@@ -1,10 +1,10 @@
 <?php
 /**
- * The Standard Internation Volume unit 
+ * The Standard Internation Volume unit
  * @see https://www.nist.gov/pml/weights-and-measures/si-units-volume Volume units
- * 
+ *
  * Volume is the measure of the 3-dimensional space occupied by matter, or enclosed by a surface, measured in cubic units. The SI unit of volume is the cubic meter (m3), which is a derived unit.
- * 
+ *
  * @author Angelo Chillemi <info@angelochillemi.com>
  */
 
@@ -39,7 +39,7 @@ return [
     */
 
     'formulas' => [
-        'mm3' => 0.000000001, 
+        'mm3' => 0.000000001,
         'cm3' => 0.000001,
         'dm3' => 0.001,
         'm3' => 1, // SI Unit
